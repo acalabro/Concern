09874893b67f57d05fc5364ebdc4bdfa2c139138
acalabro <antonello.calabro@isti.cnr.it>
2020-12-02 17:17:09 +0100

    asd

--- src/main/java/it/cnr/isti/labsedc/glimpse_reloaded/App.java
+++ src/main/java/it/cnr/isti/labsedc/glimpse_reloaded/App.java
@@ -51,7 +51,9 @@ public class App
 
 		//storage = new InfluxDBStorageController();
 	    broker = new ActiveMQBrokerManager(brokerUrl, maxMemoryUsage, maxCacheUsage, username, password);
+    	logger.debug(App.class.getSimpleName() + " is running the broker.");
     	broker.run();
+    	logger.debug(App.class.getSimpleName() + " broker launched.");
     	channelRegistry = new ChannelsManagementRegistry();
     	logger.debug("Channels Management Registry created");
     	logger.debug("PATH: " + System.getProperty("user.dir"));
